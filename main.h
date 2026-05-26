edb4038e050f5dbac7dd804c5f31cdf613822198
Jacob <jacobt0722@gmail.com>
2026-05-26 14:52:38 -0700

    removed unused .h file

--- main.h
+++
@@ -1,7 +0,0 @@
-class IChessPlayer {
-    public:
-        virtual ~IChessPlayer() = default;
-        virtual void sendCommand(const std::string& cmd) = 0;
-        virtual std::string receiveCommand() = 0;
-        virtual bool hasData() = 0; 
-};
