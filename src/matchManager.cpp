cb2234cc34247d5cd3a3789605bc2036dfffca58
Colton <76969314+becksteadc@users.noreply.github.com>
2026-06-22 13:35:22 -0700

    Rework matchManger mutex and condition_variable code to be non-global. This could support future potential for the match manger to handle more than two clients at a time.
    Additionally began the server-side socket code. Sending moves and
    awaiting responses not yet implemented.

--- src/matchManager.cpp
+++ src/matchManager.cpp
@@ -1,7 +1,7 @@
 #include "matchManager.hpp"
 
-std::mutex m;
-std::condition_variable cv;
+//std::mutex m; //refactored the globals out
+//std::condition_variable cv;
 std::string UCIResponse;
 
 int main(int argc, char **argv) {
@@ -21,12 +21,30 @@ int main(int argc, char **argv) {
     std::atomic<int> turnState;
     std::atomic<bool> gameOver = false;
     struct BoardState state;
+    std::mutex threadSyncMutex;
+    std::condition_variable mutexCondition;
     
     fenToBoardState(fen, std::ref(state));
     turnState = state.sideToMove;
 
-    engineOneThread = std::thread(engineThread, std::ref(engineOneReady), std::ref(turnState), WHITE, std::ref(gameOver));
-    engineTwoThread = std::thread(engineThread, std::ref(engineTwoReady), std::ref(turnState), BLACK, std::ref(gameOver));
+    engineOneThread = std::thread(
+        engineThread,
+        std::ref(engineOneReady),
+        std::ref(turnState),
+        WHITE,
+        std::ref(gameOver),
+        threadSyncMutex,
+        mutexCondition
+    );
+    engineTwoThread = std::thread(
+        engineThread,
+        std::ref(engineTwoReady),
+        std::ref(turnState),
+        BLACK,
+        std::ref(gameOver),
+        threadSyncMutex,
+        mutexCondition
+    );
 
     if(engineOneThread.joinable()){
         engineOneThread.join();
@@ -37,13 +55,31 @@ int main(int argc, char **argv) {
 
 }
 
-void engineThread(std::atomic<bool>& readyFlag, const std::atomic<int>& turnState, int color, std::atomic<bool>& gameOver) {
+void engineThread(
+    std::atomic<bool>& readyFlag,
+    const std::atomic<int>& turnState,
+    int color,
+    std::atomic<bool>& gameOver,
+    std::mutex m,
+    std::condition_variable cv
+) {
     int sockDesc = -1;
     if ((sockDesc = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         std::cerr << "Failed to create socket in engineThread: color = " << color << std::endl;
     }
-    //bind(
-            //TODO -- make sockets
+    struct sockaddr_in listenAddressOne = {
+        .sin_family = AF_INET,
+        .sin_port = htons(ENGINE_LISTEN_PORT[color]), //Color *must* be 0 or 1 (white, black)
+        .sin_addr = {.s_addr = INADDR_ANY} //(man 7 ip)
+    };
+    bind(sockDesc, listenAddressOne, sizeof(listenAddressOne));
+
+    const int connectionBacklogLimit = 1;
+    listen(sockDesc, connectionBacklogLimit);
+    struct sockaddr_in clientConnInfo; //filled in with call to accept()
+    socklen_t connSizeInfo = sizeof(clientConnInfo); //will be overwritten by accept()
+    accept(sockDesc, &clientConnInfo, &connSizeInfo); //block until connection
+    //Could output the contents of clientConnInfo for logging / connection debug
     
     while (true) {
         std::unique_lock lk(m);
--- src/matchManager.hpp
+++ src/matchManager.hpp
@@ -9,6 +9,10 @@
 #include <mutex>
 #include <condition_variable>
 
-void engineThread(std::atomic<bool>& readyFlag, const std::atomic<int>& turnState, int color, std::atomic<bool>& gameOver);
+void engineThread(std::atomic<bool>& readyFlag, const std::atomic<int>& turnState, int color, std::atomic<bool>& gameOver, std::mutex m, std::condition_variable cv);
+
+#define LISTEN_PORT_WHITE 3001
+#define LISTEN_PORT_BLACK 3002
+const int ENGINE_LISTEN_PORT[2] = {LISTEN_PORT_WHITE, LISTEN_PORT_BLACK};
 
 #endif // #ifndef __MATCH_MANAGER_HPP__
